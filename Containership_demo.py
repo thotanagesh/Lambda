a4fcd151fe06e49d0405d48b71198eb73f97bd18
Ravi-Teja-20 <55702515+Ravi-Teja-20@users.noreply.github.com>
2020-10-16 20:05:03 +0530

    Add files via upload

---
+++ Containership_demo.py
@@ -0,0 +1,16 @@
+class salary:
+	def __init__(self,pay):
+		self.pay=pay
+	def get_total(self):
+		return(self.pay*12)
+
+class emp:
+	def __init__(self,ipay,bonus):
+		self.ipay=ipay
+		self.bonus=bonus
+		self.obj_salary=salary(self.ipay)
+	def annual_salary(self):
+		return "Total:"+str(self.obj_salary.get_total()+self.bonus)
+
+obj_emp=emp(50000,1000)
+print(obj_emp.annual_salary())
