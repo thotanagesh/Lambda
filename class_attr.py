39ff83b3182aeffd5d621adad1eb0fc2832d6fc2
Ravi-Teja-20 <55702515+Ravi-Teja-20@users.noreply.github.com>
2020-10-17 12:08:19 +0530

    Add files via upload

---
+++ class_attr.py
@@ -0,0 +1,16 @@
+class person():
+	"""docstring for person"""
+	def __init__(self, name,age):
+		self.name = name
+		self.age=age
+		
+	def show(self):
+		print('Name:',self.name)
+		print("Age:",self.age)
+
+p=person('Ravi Teja',19)
+print(hasattr(p,p.name))
+#print(hasattr(p,p.sec))
+print(getattr(p,p.name))
+print(getattr(p,p.age,20))
+print(setattr(p,'name1','rt'))
